84170c5c8edfa09167a8d0ddbcc496c8ee1816b0
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-12-19 13:45:27 +0700

    Updated images and alphabet

--- src/cvt.sh
+++ src/cvt.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 mkdir tmp
 
+LONG_CHARS="kya kyu kyo sha shu sho cha chu cho nya nyu nyo hya hyu hyo mya myu myo rya ryu ryo gya gyu gyo ja ju jo bya byu byo pya pyu pyo"
+
 for img in $(ls|grep .png); do convert $img -crop 90x90+23+10 tmp/$img; done
-for img in $(ls | egrep "[a-z][ysh][aioeu]*"); do convert $img -crop 140x80+30+12 tmp/$img; done
-for img in shi.png chi.png tsu.png ; do convert $img -crop 90x90+23+10 tmp/$img; done
+for img in $LONG_CHARS; do convert $img.png -crop 140x80+30+12 tmp/$img.png; done
