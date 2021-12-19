806ed154694dbe7ee51809af8ab3b012957968ea
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-12-20 00:42:13 +0700

    Added hint feature

---
+++ src/js/hinting.js
@@ -0,0 +1,44 @@
+const hintElm = document.querySelector("#hint");
+
+/**
+ *
+ * @param {*} hintCountElm The element to display hint count
+ * @param {*} showHintBtn The button to trigger showing hint
+ * @param {*} maxHints Number of max hints
+ * @param {*} toggleHintElm The element to toggle hint
+ */
+function addHinting(
+  hintCountElm,
+  showHintBtn,
+  maxHints,
+  toggleHintElm,
+  opt,
+  keyCode
+) {
+  if (!opt) opt = {};
+  let setHintCount = (value) => {
+    opt.hintCount = value;
+    hintCountElm.innerHTML = opt.hintCount;
+  };
+  setHintCount(maxHints);
+  let closeHint = () => {
+    toggleHintElm.setAttribute("class", "d-hidden");
+    let arr = globalThis.bitChangedListeners;
+    arr.splice(arr.indexOf(closeHint), 1);
+  };
+  let showHint = () => {
+    if (opt.hintCount > 0) {
+      setHintCount(opt.hintCount - 1);
+      toggleHintElm.setAttribute("class", undefined);
+      globalThis.bitChangedListeners.push(closeHint);
+    }
+  };
+  showHintBtn.addEventListener("click", showHint);
+  if (typeof keyCode == "string") {
+    document.addEventListener("keypress", (event) => {
+      if (event.code === keyCode) {
+        showHint();
+      }
+    });
+  }
+}
